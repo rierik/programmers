f65a03972a97d8f4aed795fc7d268583d87195a7
hey-ri <kwonhr01@naver.com>
2023-04-23 23:07:09 +0900

    추가)n번째 원소까지

--- basic.js
+++ basic.js
@@ -28,3 +28,21 @@ function solution(n) {
   answer = String(n);
   return answer;
 }
+
+/* 
+n 번째 원소까지
+
+문제설명
+정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 n 번째 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+입출력의 예
+[2, 1, 6]의 첫 번째 원소부터 첫 번째 원소까지의 모든 원소는 [2]입니다.
+*/
+
+function solution(num_list, n) {
+  var answer = [];
+  const test = num_list;
+  answer = num_list.splice(0, n);
+
+  return answer;
+}
