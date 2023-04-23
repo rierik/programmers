809bc3c9a100c71960b6826b937213afde5acaf1
hey-ri <kwonhr01@naver.com>
2023-04-23 23:19:25 +0900

    카운트 업 구하기

--- basic.js
+++ basic.js
@@ -46,3 +46,23 @@ function solution(num_list, n) {
 
   return answer;
 }
+
+/* 
+카운트 업
+
+문제 설명
+정수 start와 end가 주어질 때, start부터 end까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+입출력의 예
+3부터 10까지의 숫자들을 담은 리스트 [3, 4, 5, 6, 7, 8, 9, 10]를 return합니다.
+*/
+
+function solution(start, end) {
+  var answer = [];
+
+  for (let i = start; i <= end; i++) {
+    answer.push(i);
+  }
+
+  return answer;
+}
