c33e4859c57c0ccb360bde136bd1f141d5fa620c
hey-ri <kwonhr01@naver.com>
2023-12-21 14:28:42 +0900

    두 연산값 비교하기

---
+++ compareTwoNumbers.js
@@ -0,0 +1,26 @@
+/* 
+연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+
+12 ⊕ 3 = 123
+3 ⊕ 12 = 312
+양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
+
+단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
+
+
+a	b	result
+2	91	364
+a ⊕ b = 291 이고, 2 * a * b = 364 입니다. 둘 중 더 큰 값은 364 이므로 364를 return 합니다.
+*/
+
+function solution(a, b) {
+  var answer = 0;
+  const strA = String(a);
+  const strB = String(b);
+  const strSum = Number(strA + strB);
+
+  const multipleNum = 2 * a * b;
+
+  answer = strSum > multipleNum ? strSum : multipleNum;
+  return answer;
+}
