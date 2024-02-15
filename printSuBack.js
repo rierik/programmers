c501f9eff1ddf1d128c7123413053efc9007b5bd
hey-ri <kwonhr01@naver.com>
2024-02-15 16:43:02 +0900

    수박수박 출력하기

---
+++ printSuBack.js
@@ -0,0 +1,19 @@
+/* 
+길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+*/
+
+function solution(num) {
+  let result = '';
+
+  for (let i = 0; i < num; i++) {
+    if (i % 2 === 0) {
+      result += '수';
+    } else {
+      result += '박';
+    }
+  }
+
+  return result;
+}
+
+console.log(solution(5));
