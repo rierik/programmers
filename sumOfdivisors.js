f1101803a135f0ef78229d9415b7bd4448299cd2
hey-ri <kwonhr01@naver.com>
2024-02-15 14:10:40 +0900

    약수의 합

--- sumOfdivisors.js
+++ sumOfdivisors.js
@@ -11,3 +11,12 @@ function solution(n) {
   result.map((d) => (answer += d));
   return answer;
 }
+
+/*  */
+function solution(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) sum += i;
+  }
+  return sum;
+}
