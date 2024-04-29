18e420a89ec3c1ad5ed1cfd9fc3249f34b48cf4f
hey-ri <kwonhr01@naver.com>
2024-04-29 10:39:09 +0900

    문자열 내의 p와 y의 개수 다른풀이

--- numberOfpandyInString.js
+++ numberOfpandyInString.js
@@ -39,4 +39,30 @@ function solution(s) {
   return answer;
 }
 
-solution('pyy');
+solution('kkk');
+
+//
+function numPY(s) {
+  return s.match(/p/gi).length == s.match(/y/gi).length;
+}
+//numPY의 의견
+/* 
+p와 y둘다 없을 때 에러..
+
+p와 y가 둘 다 없을 때는 match 함수는 null을 반환합니다. 그래서 null은 length함수를 가지고 있지 않기에, length 함수가 없다는 undefined 에러가 나기 때문에. 변수를 선언하고, 해당 match의 결과가 null일 때는 그 변수에 0을 넣어준 뒤 그 변수를 비교하는 방식으로 해결해야 예외처리를 완벽하게 할 수 있습니다.
+*/
+
+//
+function numPY(s) {
+  //함수를 완성하세요
+  console.log('s.toUpperCase().spli', s.toUpperCase().split('Y'));
+
+  return s.toUpperCase().split('P').length === s.toUpperCase().split('Y').length;
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+// console.log(numPY('pPoooyY'));
+// console.log(numPY('Pyy'));
+
+//numPY의 의견
+// 와 신기하다 split에 separator줘서 나누면 리턴되는 배열의 길이는 일치되는 개수 + 1일테니까 이렇게도 비교가 가능하군요
