9c9a85725fe1749669de76ead74feeb8007497dc
hey-ri <kwonhr01@naver.com>
2024-07-09 15:48:22 +0900

    겹 치는 선분의 길이

---
+++ LengthOfOverlappingLine.js
@@ -0,0 +1,28 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120876
+function solution(lines) {
+  var answer = 0;
+  let lineMap = new Array(200).fill(0);
+
+  for (let i = 0; i < lines.length; i++) {
+    let prev = lines[i][0];
+    let next = lines[i][1];
+
+    for (let j = prev; j < next; j++) {
+      lineMap[j + 100] += 1;
+    }
+  }
+
+  for (let i in lineMap) {
+    if (lineMap[i] > 1) {
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
+
+solution([
+  [0, 5],
+  [3, 9],
+  [1, 10],
+]);
