d2d4fb29f1861403e85e00f4ae46b80d542a197d
kwonhr <kwonhr@easywith.com>
2024-07-10 13:54:15 +0900

    주사위게임3 다른 풀이

--- diceGame.js
+++ diceGame.js
@@ -49,3 +49,26 @@ solution(4, 1, 4, 4);
 solution(6, 3, 6, 3);
 solution(2, 5, 2, 6);
 solution(2, 4, 5, 7);
+
+//다른 풀이
+function solution(a, b, c, d) {
+  if (a === b && a === c && a === d) return 1111 * a;
+
+  if (a === b && a === c) return (10 * a + d) ** 2;
+  if (a === b && a === d) return (10 * a + c) ** 2;
+  if (a === c && a === d) return (10 * a + b) ** 2;
+  if (b === c && b === d) return (10 * b + a) ** 2;
+
+  if (a === b && c === d) return (a + c) * Math.abs(a - c);
+  if (a === c && b === d) return (a + b) * Math.abs(a - b);
+  if (a === d && b === c) return (a + b) * Math.abs(a - b);
+
+  if (a === b) return c * d;
+  if (a === c) return b * d;
+  if (a === d) return b * c;
+  if (b === c) return a * d;
+  if (b === d) return a * c;
+  if (c === d) return a * b;
+
+  return Math.min(a, b, c, d);
+}
