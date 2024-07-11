a0d61673a70ca78295ae871acea25255495f5319
kwonhr <kwonhr@easywith.com>
2024-07-11 13:35:34 +0900

    자연수 뒤집어 배열로 만들기 다른풀이

--- reverseNumber.js
+++ reverseNumber.js
@@ -7,3 +7,23 @@ function solution(n) {
 }
 
 console.log(solution(12345));
+
+function solution(n) {
+  // 문자풀이
+  return (n + '')
+    .split('')
+    .reverse()
+    .map((v) => parseInt(v));
+}
+
+function solution(n) {
+  // 숫자풀이
+  var arr = [];
+
+  do {
+    arr.push(n % 10);
+    n = Math.floor(n / 10);
+  } while (n > 0);
+
+  return arr;
+}
