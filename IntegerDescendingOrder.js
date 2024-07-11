5dd0114dab593a3095b157a9b90288f32cfe9050
kwonhr <kwonhr@easywith.com>
2024-07-11 14:36:19 +0900

    정수 내림차순으로 배치하기 다른풀이

--- IntegerDescendingOrder.js
+++ IntegerDescendingOrder.js
@@ -11,3 +11,22 @@ function solution(n) {
 }
 
 console.log(solution(118372));
+
+//다른풀이
+function solution(n) {
+  //숫자가 분명히 더 빠름
+  var nums = [];
+  do {
+    nums.push(n % 10);
+    n = Math.floor(n / 10);
+  } while (n > 0);
+
+  return nums.sort((a, b) => b - a).join('') * 1;
+  //문자는 느림
+  return (
+    (n + '')
+      .split('')
+      .sort((a, b) => b - a)
+      .join('') * 1
+  );
+}
