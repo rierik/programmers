7171ceff803bd6871e9b0bca881bfdaff8e38a19
kwonhr <kwonhr@easywith.com>
2024-07-12 14:12:34 +0900

    음양 구하기 다른 풀이

--- AddNegativePositiveNumbers.js
+++ AddNegativePositiveNumbers.js
@@ -13,5 +13,22 @@ function solution(absolutes, signs) {
   return answer;
 }
 
+// 다른 풀이
+function solution(absolutes, signs) {
+  return absolutes.reduce((acc, val, i) => acc + val * (signs[i] ? 1 : -1), 0);
+}
+
+function solution(absolutes, signs) {
+  let answer = 0;
+  absolutes.forEach((v, i) => {
+    if (signs[i]) {
+      answer += v;
+    } else {
+      answer -= v;
+    }
+  });
+  return answer;
+}
+
 console.log(solution([4, 7, 12], [true, false, true])); // 9
 console.log(solution([1, 2, 3], [false, false, true])); // 0
