a51a924572ae1a9630b6cbb6a193b33d2f8fe740
kwonhr <kwonhr@easywith.com>
2024-07-12 14:07:38 +0900

    음양 구하기 풀이 수정

--- AddNegativePositiveNumbers.js
+++ AddNegativePositiveNumbers.js
@@ -1,17 +1,16 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/76501
 
 function solution(absolutes, signs) {
-  let negative = 0;
-  let positive = 0;
+  var answer = 0;
 
   for (i = 0; i < absolutes.length; i++) {
     if (signs[i]) {
-      positive += absolutes[i];
+      answer += absolutes[i];
     } else {
-      negative += absolutes[i] * -1;
+      answer += absolutes[i] * -1;
     }
   }
-  return positive + negative;
+  return answer;
 }
 
 console.log(solution([4, 7, 12], [true, false, true])); // 9
