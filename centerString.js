8aeeda80918440d5e813890fdaf6994f4f4eb9a5
kwonhr <kwonhr@easywith.com>
2024-07-12 17:32:59 +0900

    가운데 글자 가져오기

---
+++ centerString.js
@@ -0,0 +1,9 @@
+function solution(s) {
+  var answer = '';
+  if (s.length % 2 != 0) {
+    answer = s.slice(Math.floor(s.length / 2), Math.floor(s.length / 2) + 1);
+  } else {
+    answer = s.slice(Math.floor(s.length / 2) - 1, Math.floor(s.length / 2) + 1);
+  }
+  return answer;
+}
