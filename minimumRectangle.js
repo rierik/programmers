046d6759ad33c856afc17f8b4f1d4461badd2dc8
hey-ri <kwonhr01@naver.com>
2024-07-16 14:05:48 +0900

    최소 직사각형

---
+++ minimumRectangle.js
@@ -0,0 +1,20 @@
+function solution(sizes) {
+  var w_max = [];
+  var h_max = [];
+  for (var i = 0; i < sizes.length; i++) {
+    sizes[i].sort((a, b) => b - a);
+    w_max.push(sizes[i][0]);
+    h_max.push(sizes[i][1]);
+  }
+  return Math.max(...w_max) * Math.max(...h_max);
+}
+
+console.log(
+  solution([
+    [60, 50],
+    [30, 70],
+    [60, 30],
+    [80, 40],
+  ]),
+);
+//40000
