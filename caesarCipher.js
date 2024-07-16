f79591ceee089520be9915968cdaf793cf05357c
kwonhr <kwonhr@easywith.com>
2024-07-16 16:10:29 +0900

    시저암호

---
+++ caesarCipher.js
@@ -0,0 +1,23 @@
+function solution(s, n) {
+  var answer = '';
+  let upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let lower = 'abcdefghijklmnopqrstuvwxyz';
+
+  var answer = '';
+  for (var i = 0; i < s.length; i++) {
+    var text = s[i];
+    if (text == ' ') {
+      answer += ' ';
+      continue;
+    }
+    console.log(text);
+    var textArr = upper.includes(text) ? upper : lower;
+    var index = textArr.indexOf(text) + n;
+    console.log(index);
+    if (index >= textArr.length) index -= textArr.length;
+    answer += textArr[index];
+  }
+  return answer;
+}
+
+console.log(solution('a B z', 4));
