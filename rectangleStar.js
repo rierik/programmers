45b0167a2ea9acd6ed5595ef0c89d4bd49e5ea95
hey-ri <kwonhr01@naver.com>
2024-07-16 16:29:12 +0900

    직사각형 별찍기 다른풀이

--- rectangleStar.js
+++ rectangleStar.js
@@ -14,3 +14,15 @@ process.stdin.on('data', (data) => {
 
   console.log(answer);
 });
+
+//다른 풀이
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  const n = data.split(' ');
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  const row = '*'.repeat(a);
+  for (let i = 0; i < b; i++) {
+    console.log(row);
+  }
+});
