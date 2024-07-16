0486900089f1f1c791786b61173dfbd90463e868
hey-ri <kwonhr01@naver.com>
2024-07-16 14:28:35 +0900

    최소 직사각형 다른풀이

--- minimumRectangle.js
+++ minimumRectangle.js
@@ -9,6 +9,7 @@ function solution(sizes) {
   return Math.max(...w_max) * Math.max(...h_max);
 }
 
+//다른풀이
 function solution(sizes) {
   const [hor, ver] = sizes.reduce(
     ([h, v], [a, b]) => {
@@ -18,6 +19,17 @@ function solution(sizes) {
     [0, 0],
   );
   return hor * ver;
+}
+
+function solution(sizes) {
+  const rotated = sizes.map(([w, h]) => (w < h ? [h, w] : [w, h]));
+
+  let maxSize = [0, 0];
+  rotated.forEach(([w, h]) => {
+    if (w > maxSize[0]) maxSize[0] = w;
+    if (h > maxSize[1]) maxSize[1] = h;
+  });
+  return maxSize[0] * maxSize[1];
 }
 
 console.log(
