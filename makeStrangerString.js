8192682573c0802d7d3897957271b7a0edc05a7d
hey-ri <kwonhr01@naver.com>
2024-07-18 11:02:29 +0900

    이상한 문자 만들기 다른 풀이(문제 변경으로 수정)

--- makeStrangerString.js
+++ makeStrangerString.js
@@ -54,7 +54,7 @@ function toWeirdCase(s) {
     .map((i) =>
       i
         .split('')
-        .map((j, key) => (key % 2 === 0 ? j.toUpperCase() : j))
+        .map((j, key) => (key % 2 === 0 ? j.toUpperCase() : j.toLowerCase()))
         .join(''),
     )
     .join(' ');
