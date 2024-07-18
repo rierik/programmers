c882043064806fa0c43ae175cb6ab8661d15a70a
hey-ri <kwonhr01@naver.com>
2024-07-18 11:00:04 +0900

    이상한 문자 만들기 다른 풀이

--- makeStrangerString.js
+++ makeStrangerString.js
@@ -3,7 +3,7 @@ function solution(s) {
 
   const splitString = s.split(' ');
 
-  console.log(splitString);
+  // console.log(splitString);
   splitString.map((s, i) => {
     // console.log(s, '\n', i);
     const oneSpell = s.split('');
@@ -20,6 +20,44 @@ function solution(s) {
   });
   console.log(answer);
   return answer;
+}
+
+//다른풀이
+function toWeirdCase(s) {
+  /* 
+  s.toUpperCase():
+
+  "HELLO WORLD"
+  .replace(/(\w)(\w)/g, function (a) { ... }):
+
+  정규표현식 (\w)(\w)는 "HELLO WORLD"에서 두 개의 연속된 문자를 찾습니다:
+  "HE", "LL", "OW", "OR", "LD"
+  각 쌍에 대해 첫 번째 문자를 대문자로, 두 번째 문자를 소문자로 변환합니다:
+  "HE" -> "He"
+  "LL" -> "Ll"
+  "OW" -> "Ow"
+  "OR" -> "Or"
+  "LD" -> "Ld"
+  결과 문자열:
+
+  "HeLlO WoRlD"
+  */
+  //함수를 완성해주세요
+  return s.toUpperCase().replace(/(\w)(\w)/g, function (a) {
+    return a[0].toUpperCase() + a[1].toLowerCase();
+  });
+}
+
+function toWeirdCase(s) {
+  return s
+    .split(' ')
+    .map((i) =>
+      i
+        .split('')
+        .map((j, key) => (key % 2 === 0 ? j.toUpperCase() : j))
+        .join(''),
+    )
+    .join(' ');
 }
 
 console.log(solution('try    hello world'));
