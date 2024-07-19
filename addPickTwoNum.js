1dc8c6e1d1d344254f24bbebd61b8fa1f752cd6f
hey-ri <kwonhr01@naver.com>
2024-07-19 10:35:24 +0900

    두 개 뽑아서 더하기

---
+++ addPickTwoNum.js
@@ -0,0 +1,22 @@
+function solution(numbers) {
+  var answer = [];
+  let temp = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (i !== j) {
+        temp.push(numbers[i] + numbers[j]);
+      }
+    }
+  }
+
+  const set = new Set(temp);
+  answer = [...set];
+
+  answer.sort(function (a, b) {
+    return a - b;
+  });
+
+  return answer;
+}
+
+console.log(solution([2, 1, 3, 4, 1]));
