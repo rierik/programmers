505dcb4da93ad8123d68fa669c4f96c58f816c8e
kwonhr <kwonhr@easywith.com>
2024-07-19 10:41:58 +0900

    두 개 뽑아서 더하기 다른풀이

--- addPickTwoNum.js
+++ addPickTwoNum.js
@@ -18,3 +18,38 @@ function solution(numbers) {
 }
 
 console.log(solution([2, 1, 3, 4, 1]));
+
+//다른풀이
+function solution(numbers) {
+  var answer = [];
+  //들어온 배열의 요소를 확인한다.
+  //배열의 두개 요소를 뽑아 덧셈한 모든 경우의 수를 구한다.
+  //중복된 값은 제거한다.
+  //배열값을 리턴한다.
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (answer.indexOf(numbers[i] + numbers[j]) === -1) {
+        answer.push(numbers[i] + numbers[j]);
+      }
+    }
+  }
+
+  return answer.sort((a, b) => a - b);
+}
+
+function solution(numbers) {
+  var answer = [];
+  numbers = numbers.sort();
+  console.log(numbers);
+  for (var i = 0; i < numbers.length; i++) {
+    for (var k = i + 1; k < numbers.length; k++) {
+      if (!answer.includes(numbers[i] + numbers[k])) {
+        answer.push(numbers[i] + numbers[k]);
+      }
+    }
+  }
+  answer = answer.sort(function (a, b) {
+    return a - b;
+  });
+  return answer;
+}
