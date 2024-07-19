a4e0550ec1b4e8dc0c8dace934e083cd816b8f3e
hey-ri <kwonhr01@naver.com>
2024-07-19 16:46:30 +0900

    푸드파이터 대회

---
+++ foodFightCompetition.js
@@ -0,0 +1,27 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/134240
+function solution(food) {
+  var answer = '';
+  let temp = [];
+  food.reduce((a, c, i) => {
+    console.log('dddd', a, c, i);
+    if (i > 0) {
+      if (c >= 2) temp.push({ food: i, count: c });
+    } else {
+      temp.push({ food: i, count: c });
+    }
+  }, []);
+
+  let arr = [];
+  temp.filter((fc, i) => {
+    if (fc.count > 1) {
+      console.log(fc.count);
+      arr.push(fc.food.toString().repeat(Math.floor(fc.count / 2)));
+    }
+  });
+
+  console.log('arr', arr);
+
+  return (answer = arr.join('') + '0' + arr.reverse().join(''));
+}
+
+console.log(solution([1, 7, 1, 2])); //"1223330333221"
