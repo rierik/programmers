6f5cff81b68ed8c9264756babe949efb4652bef7
hey-ri <kwonhr01@naver.com>
2024-07-24 14:24:34 +0900

    콜라 문제 다른풀이

--- cokeProblem.js
+++ cokeProblem.js
@@ -1,14 +1,36 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/132267?language=javascript
-function solution(a, b, n) {
+function solutions(give, take, init) {
   // 문제 해석하여 들어오는 인자를 대입해보면 a = 가져다줘야 하는 병 수 2 b = 돌려받을 수 있는 병 수 1 n = 20 내가 가지고 있는 콜라병 수
   var answer = 0;
 
-  while (n >= a) {
-    answer += Math.floor(n / a) * b;
-    n = Math.floor(n / a) * b + Math.floor(n % a);
+  while (init >= give) {
+    answer += Math.floor(init / give) * take;
+    init = Math.floor(init / give) * take + Math.floor(init % give);
   }
 
   return answer;
 }
 
-console.log(solution(2, 1, 20));
+//다른 풀이
+solutions = (give, take, init) => Math.floor(Math.max(init - take, 0) / (give - take)) * take;
+
+function solution(give, take, init) {
+  let answer = 0;
+  while (init >= give) {
+    answer += parseInt(init / give) * take;
+    init = parseInt(init / give) * take + (init % give);
+  }
+  return answer;
+}
+
+function solution(give, take, init) {
+  let answer = 0;
+  for (let i = give; i <= init; i += give) {
+    answer += take;
+    init += take;
+
+    console.log(i, give, take, init);
+  }
+  return answer;
+}
+console.log(solution(3, 2, 20));
