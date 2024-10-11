40564268db2500243d9102ba2242e9d5f44f47b6
hey-ri <kwonhr01@naver.com>
2024-10-11 10:54:19 +0900

    최댓값과 최솟값 다른 풀이

--- MaxnumAndMinnum.js
+++ MaxnumAndMinnum.js
@@ -10,6 +10,13 @@ function solution(s) {
   return answer;
 }
 
+//좀 더 깔끔한 풀이
+function solution(s) {
+  const arr = s.split(' ');
+
+  return Math.min(...arr) + ' ' + Math.max(...arr);
+}
+
 console.log(solution('1 2 3 4')); //	"1 4"
 console.log(solution('-1 -2 -3 -4')); //	"-4 -1"
 console.log(solution('-1 -1')); //"-1 -1"
