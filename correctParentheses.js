e9f38c6791910cca5909572e50b57bc7ce9b7e11
hey-ri <kwonhr01@naver.com>
2024-10-11 10:35:27 +0900

    올바른 괄호 다른 풀이

--- correctParentheses.js
+++ correctParentheses.js
@@ -23,6 +23,19 @@ function solution(s) {
   return leftParentheseNum === rightParentheseNum;
 }
 
+//좀 더 깔끔한 풀이
+function solution(s) {
+  let cnt = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    s[i] === '(' ? (cnt += 1) : (cnt += -1);
+    if (cnt < 0) {
+      break;
+    }
+  }
+  return cnt === 0 ? true : false;
+}
+
 console.log(solution('())((()))(()'));
 console.log(solution('((((())()(()))())'));
 console.log(solution(')()('));
