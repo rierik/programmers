3386e8a5a80de570c0c1b47f937be8e00c9bad3e
hey-ri <kwonhr01@naver.com>
2024-10-11 12:04:29 +0900

    다음 큰 숫자 다른 풀이

--- nextBigNum.js
+++ nextBigNum.js
@@ -13,3 +13,18 @@ function solution(n) {
 console.log(solution(78)); //83
 console.log(solution(15)); //24
 console.log(solution(13)); //24
+
+function solution(n) {
+  let arr = n.toString(2).split(''); // 2진법으로 변환해 배열 만들기
+  let cnt = arr.filter((one) => one === '1').length; // 1의 개수 세기
+  let cnt2;
+  while (cnt !== cnt2) {
+    //cnt와 cnt2의 크기가 같을 때 종료
+    ++n; // 다음 큰 숫자
+    cnt2 = n
+      .toString(2)
+      .split('')
+      .filter((one) => one === '1').length; // 1의 개수 세기
+  }
+  return n;
+}
