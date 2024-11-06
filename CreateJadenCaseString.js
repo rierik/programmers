f6d9fc8d8fa2417a5522d0a19b4b313830720106
hey-ri <kwonhr01@naver.com>
2024-11-06 15:40:27 +0900

    JadenCase 문자열 만들기 최적화 풀이 (통과 풀이)

--- CreateJadenCaseString.js
+++ CreateJadenCaseString.js
@@ -1,3 +1,4 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12951
 function solution(s) {
   var answer = '';
   let arr = [];
@@ -6,14 +7,27 @@ function solution(s) {
 
   for (let i = 0; i < arr.length; i++) {
     const spellArr = arr[i].split('');
-    let spell = '';
     spellArr[0] = spellArr[0].toUpperCase();
+
     for (let j = 1; j < spellArr.length; j++) {
       spellArr[j] = spellArr[j].toLowerCase();
     }
+
     arr[i] = spellArr.join('');
   }
+
   return arr.join(' ');
+}
+
+//최적화 실패로 다시 푼 풀이
+function solution(s) {
+  return s
+    .split(' ')
+    .map((word) => {
+      if (word.length === 0) return word;
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join(' ');
 }
 
 console.log(solution('3people unFollowed me')); //3people Unfollowed Me
