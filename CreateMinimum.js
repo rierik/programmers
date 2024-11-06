0e771cd3c6172699da358cf6f5c80144532bce55
hey-ri <kwonhr01@naver.com>
2024-11-06 14:28:22 +0900

    최솟값 만들기 다른풀이

--- CreateMinimum.js
+++ CreateMinimum.js
@@ -14,3 +14,10 @@ function solution(A, B) {
 
 console.log(solution([1, 4, 2], [5, 4, 4])); // 29
 // console.log(solution([1, 2], [3, 4])); // 10
+
+//다른 사람 풀이
+function solution(A, B) {
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+  return A.reduce((total, val, idx) => total + val * B[idx], 0);
+}
