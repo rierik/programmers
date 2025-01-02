dd2677ffc1c4156f047598effc2da33fff755e22
hey-ri <kwonhr01@naver.com>
2025-01-02 18:03:26 +0900

    숫자의 표현 약수의 개수와 표현할 수 있는 수의 개수가 맞다고 하는 데 이해를 완전하게 하지는 못함

--- expressionofNumber.js
+++ expressionofNumber.js
@@ -19,3 +19,13 @@ function solution(n) {
 
   return count;
 }
+
+//주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수는 주어진 수의 홀수 약수의 개수와 같다고 한다.
+function solution(num) {
+  var answer = 0;
+
+  for (var i = 1; i <= num; i++) {
+    if (num % i == 0 && i % 2 == 1) answer++;
+  }
+  return answer;
+}
