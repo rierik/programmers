e97cd2b5f94d6f47b2385641dad214071d25c438
hey-ri <kwonhr01@naver.com>
2025-01-17 12:06:13 +0900

    점프와 순간이동
    효율성테스트 성공 but 내 온전한 실력 x

--- JumpimgandTeleportation.js
+++ JumpimgandTeleportation.js
@@ -1,20 +1,26 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12980
+// function solution(n) {
+//   var ans = 0;
+
+//   while (n !== 1) {
+//     console.log('n', n);
+//     if (n % 2 === 0) {
+//       n = n / 2;
+//     } else {
+//       ans++;
+//       n = (n - 1) / 2;
+//     }
+//   }
+
+//   return ans + 1;
+// }
+
 function solution(n) {
-  var ans = 0;
-
-  while (n !== 1) {
-    console.log('n', n);
-    if (n % 2 === 0) {
-      n = n / 2;
-    } else {
-      ans++;
-      n = (n - 1) / 2;
-    }
-  }
-
-  return ans + 1;
+  if (n === 0) return 0; // 종료 조건
+  console.log('n', n, Math.floor(n / 2));
+  return (n % 2) + solution(Math.floor(n / 2));
 }
 
-// console.log(solution(5)); //2
+console.log(solution(5)); //2
 // console.log(solution(6)); //2
 // console.log(solution(5000)); //5
