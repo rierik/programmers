55fc3e8265e33ba079e4bb672b5002072d85bbfa
hey-ri <kwonhr01@naver.com>
2025-02-26 17:24:24 +0900

    기능 개발 다른 사람 풀이

--- FunctionDevelopment.js
+++ FunctionDevelopment.js
@@ -31,4 +31,24 @@ function solution(progresses, speeds) {
   return answer;
 }
 
+//다른 풀이
+function solution(progresses, speeds) {
+  let answer = [0];
+  let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+  let maxDay = days[0];
+
+  console.log(days); //[7,3,9]
+
+  for (let i = 0, j = 0; i < days.length; i++) {
+    if (days[i] <= maxDay) {
+      answer[j] += 1;
+    } else {
+      maxDay = days[i];
+      answer[++j] = 1;
+    }
+  }
+
+  return answer;
+}
+
 console.log(solution([93, 30, 55], [1, 30, 5])); //[2, 1]
