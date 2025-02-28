9cc4aa6259ba952be0baf2a72b4afcb572ec9ac3
hey-ri <kwonhr01@naver.com>
2025-02-28 17:44:12 +0900

    피로도
    완전한 내 코드 x

---
+++ Tiredness.js
@@ -0,0 +1,32 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/87946
+function solution(k, dungeons) {
+  let maxCount = 0; // 최대 탐험 가능한 던전 개수 저장
+
+  const dungeonCount = dungeons.length;
+  const visited = new Array(dungeonCount).fill(false); // 방문 여부 체크
+
+  function explore(currentFatigue, exploredCount) {
+    maxCount = Math.max(maxCount, exploredCount); // 최대값 갱신
+
+    for (let i = 0; i < dungeonCount; i++) {
+      let [minFatigue, consumeFatigue] = dungeons[i];
+
+      if (visited[i] || currentFatigue < minFatigue) continue; // 탐험 불가능한 경우 패스
+
+      visited[i] = true; // 탐험 시작 (방문 체크)
+      explore(currentFatigue - consumeFatigue, exploredCount + 1); // 다음 던전 탐험
+      visited[i] = false; // 원래 상태로 되돌리기 (백트래킹)
+    }
+  }
+
+  explore(k, 0); // 탐험 시작
+  return maxCount; // 최댓값 반환
+}
+
+console.log(
+  solution(80, [
+    [50, 40],
+    [30, 10],
+    [80, 20],
+  ]),
+); //3
