bed21af72e4701e6c7151bf27679b56f28e17002
hey-ri <kwonhr01@naver.com>
2025-03-06 17:46:38 +0900

    전화번호 목록 체점 결과 1가지 케이스 불통으로 코드 수정

--- PhoneNumberList.js
+++ PhoneNumberList.js
@@ -4,7 +4,7 @@ function solution(phone_book) {
   phone_book.sort();
 
   for (let i = 0; i < phone_book.length - 1; i++) {
-    if (phone_book[i + 1].includes(phone_book[i])) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
       return false;
     }
   }
