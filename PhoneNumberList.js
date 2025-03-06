083f8cb036ba882b82ea91ae8a84278b60f2e524
hey-ri <kwonhr01@naver.com>
2025-03-06 17:36:49 +0900

    전화번호 목록

---
+++ PhoneNumberList.js
@@ -0,0 +1,17 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42577
+
+function solution(phone_book) {
+  phone_book.sort();
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].includes(phone_book[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(solution(['119', '97674223', '1195524421'])); //false
+// console.log(solution(['123', '456', '789'])); //true
+//console.log(solution(['12', '123', '1235', '567', '88'])); //false
