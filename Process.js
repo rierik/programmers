fe55a75ea9f9ca5e297251f617d8242a05658da9
hey-ri <kwonhr01@naver.com>
2025-03-07 14:44:49 +0900

    다른 사람 풀이

--- Process.js
+++ Process.js
@@ -26,3 +26,27 @@ function solution(priorities, location) {
 
 console.log(solution([2, 1, 3, 2], 2)); //1
 // console.log(solution([1, 1, 9, 1, 1, 1], 0)); //5
+
+//다른 사람 풀이
+function solution(priorities, location) {
+  var arr = priorities.map((priority, index) => {
+    return {
+      index: index,
+      priority: priority,
+    };
+  });
+
+  var queue = [];
+
+  while (arr.length > 0) {
+    var firstEle = arr.shift();
+    var hasHighPriority = arr.some((ele) => ele.priority > firstEle.priority);
+    if (hasHighPriority) {
+      arr.push(firstEle);
+    } else {
+      queue.push(firstEle);
+    }
+  }
+
+  return queue.findIndex((queueEle) => queueEle.index === location) + 1;
+}
