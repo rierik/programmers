2caba81a6ba6b8cb35ed597c632cf79556c625cf
hey-ri <kwonhr01@naver.com>
2025-03-18 16:23:40 +0900

    롤케이크 자르기
    다른사람 풀이

--- CutRollCake.js
+++ CutRollCake.js
@@ -31,3 +31,31 @@ function solution(topping) {
 
 // console.log(solution([1, 2, 1, 3, 1, 4, 1, 2])); // 2
 console.log(solution([1, 2, 3, 1, 4])); // 0
+
+//다른 사람 풀이
+function solution(topping) {
+  const a = new Set();
+  const b = {};
+
+  let answer = 0;
+  let check = 0;
+
+  for (let i = 0; i < topping.length; i++) {
+    if (b[topping[i]]) {
+      b[topping[i]]++;
+    } else {
+      b[topping[i]] = 1;
+      check++;
+    }
+  }
+
+  for (let i = 0; i < topping.length; i++) {
+    a.add(topping[i]);
+    b[topping[i]]--;
+
+    if (!b[topping[i]]) check--;
+    if (a.size === check) answer++;
+  }
+
+  return answer;
+}
