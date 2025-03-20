f559c8974eec1caa72dd4d0a5c2d3dcd4791770d
hey-ri <kwonhr01@naver.com>
2025-03-20 17:34:51 +0900

    뒤에 있는 큰 수 찾기
    제미니 도움코드가 틀려서 다른 사람 코드로 풀이 변경

--- FindBigNumberBehind.js
+++ FindBigNumberBehind.js
@@ -1,4 +1,5 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/154539
+//시간 초과 에러
 function solution(numbers) {
   var answer = [];
 
@@ -20,21 +21,18 @@ function solution(numbers) {
 }
 console.log(solution([2, 3, 3, 5])); //[3, 5, 5, -1]
 
+//다른 사람 풀이
 function solution(numbers) {
-  const n = numbers.length;
-  const answer = new Array(n).fill(-1); // 결과를 저장할 배열 초기화
-  const stack =; // 스택 초기화
-
-  for (let i = 0; i < n; i++) {
-    // 스택이 비어있지 않고, 스택의 top에 해당하는 numbers의 값이 현재 numbers[i]보다 작다면
-    while (stack.length > 0 && numbers[stack[stack.length - 1]] < numbers[i]) {
-      // 스택에서 index를 꺼내고, 해당 index의 answer 값을 현재 numbers[i]로 설정합니다.
-      // 이는 스택 top에 있던 숫자의 다음 큰 수가 numbers[i]임을 의미합니다.
-      answer[stack.pop()] = numbers[i];
+  var answer = Array(numbers.length);
+  var check = [0];
+  for (var i = 1; i < numbers.length; i++) {
+    while (check.length && numbers[check[check.length - 1]] < numbers[i]) {
+      answer[check.pop()] = numbers[i];
     }
-    // 현재 index를 스택에 push합니다.
-    stack.push(i);
+    check.push(i);
   }
-
+  while (check.length) {
+    answer[check.pop()] = -1;
+  }
   return answer;
 }
