26c683a10bb730c0c1c91e1e1fdca811f760cdd1
hey-ri <kwonhr01@naver.com>
2025-04-23 17:57:23 +0900

    땅따먹기

---
+++ EatTheLand.js
@@ -0,0 +1,22 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12913
+function solution(land) {
+  for (let i = 1; i < land.length; i++) {
+    const prev = [...land[i - 1]]; // i-1번째 줄을 복사해놓고
+    for (let j = 0; j < land[i].length; j++) {
+      // 현재 행의 각 열에 대해 이전 행의 최대값을 더합니다.
+      land[i][j] += Math.max(...prev.filter((_, index) => index !== j));
+    }
+  }
+  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+  console.log('Hello Javascript');
+
+  return Math.max(...land[land.length - 1]); // 마지막 줄에서 최대값
+}
+
+console.log(
+  solution([
+    [1, 2, 3, 5],
+    [5, 6, 7, 8],
+    [4, 3, 2, 1],
+  ]),
+); //16
