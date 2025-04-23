6f8f76d20113e85ebb119523c33d4408ab11d3ac
hey-ri <kwonhr01@naver.com>
2025-04-23 15:50:21 +0900

    더 맵게
    다른 풀이

--- MoreSpicy.js
+++ MoreSpicy.js
@@ -17,3 +17,68 @@ function solution(scoville, K) {
 }
 
 console.log(solution([2, 1, 3, 9, 10, 12], 7)); //2
+
+// 다른 사람 풀이
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    let i = this.heap.length - 1;
+    while (i > 0) {
+      const parent = Math.floor((i - 1) / 2);
+      if (this.heap[parent] <= this.heap[i]) break;
+      [this.heap[parent], this.heap[i]] = [this.heap[i], this.heap[parent]];
+      i = parent;
+    }
+  }
+
+  pop() {
+    const min = this.heap[0];
+    const last = this.heap.pop();
+    if (this.heap.length === 0) return min;
+    this.heap[0] = last;
+
+    let i = 0;
+    while (true) {
+      let left = i * 2 + 1;
+      let right = i * 2 + 2;
+      let smallest = i;
+
+      if (left < this.heap.length && this.heap[left] < this.heap[smallest]) smallest = left;
+      if (right < this.heap.length && this.heap[right] < this.heap[smallest]) smallest = right;
+
+      if (smallest === i) break;
+      [this.heap[i], this.heap[smallest]] = [this.heap[smallest], this.heap[i]];
+      i = smallest;
+    }
+
+    return min;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+}
+
+function solution(scoville, K) {
+  const heap = new MinHeap();
+  scoville.forEach((s) => heap.insert(s));
+
+  let count = 0;
+
+  while (heap.size() >= 2 && heap.peek() < K) {
+    const first = heap.pop();
+    const second = heap.pop();
+    heap.insert(first + second * 2);
+    count++;
+  }
+
+  return heap.peek() >= K ? count : -1;
+}
