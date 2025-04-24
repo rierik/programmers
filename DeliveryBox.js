a77b71444cc818aa799e4fc313a6ef0cedefeb18
hey-ri <kwonhr01@naver.com>
2025-04-24 17:56:39 +0900

    택배상자 효율적이게 계산하기

--- DeliveryBox.js
+++ DeliveryBox.js
@@ -16,4 +16,26 @@ function solution(order) {
   return answer;
 }
 
+//뒤집어서 풀어서 효율 좋게 만들기
+// shift와 unshift보다 push와 pop이 더 효율적이다.
+function solution(order) {
+  let answer = 0;
+  const subBelt = [];
+  let box = 1;
+
+  while (box <= order.length) {
+    subBelt.push(box);
+
+    console.log(subBelt);
+
+    while (subBelt.length > 0 && subBelt[arr.length - 1] === order[answer]) {
+      subBelt.pop();
+      answer++;
+    }
+
+    box++;
+  }
+
+  return answer;
+}
 console.log(solution([5, 4, 3, 2, 1])); // 2
