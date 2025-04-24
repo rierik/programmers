cf5fe5451e5b58cbaa06b6f360f0c136bc7c2831
hey-ri <kwonhr01@naver.com>
2025-04-24 10:29:48 +0900

    땅따먹기 다른 사람 풀이

--- EatTheLand.js
+++ EatTheLand.js
@@ -20,3 +20,21 @@ console.log(
     [4, 3, 2, 1],
   ]),
 ); //16
+
+//다른 사람 풀이
+function solution(land) {
+  let answer = 0;
+  const dp = [land[0]];
+
+  for (let i = 1; i < land.length; i++) {
+    const column = [];
+
+    column.push(Math.max(dp[i - 1][1], dp[i - 1][2], dp[i - 1][3]) + land[i][0]);
+    column.push(Math.max(dp[i - 1][0], dp[i - 1][2], dp[i - 1][3]) + land[i][1]);
+    column.push(Math.max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][3]) + land[i][2]);
+    column.push(Math.max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]) + land[i][3]);
+    dp.push(column);
+  }
+
+  return Math.max(dp[land.length - 1][0], dp[land.length - 1][1], dp[land.length - 1][2], dp[land.length - 1][3]);
+}
