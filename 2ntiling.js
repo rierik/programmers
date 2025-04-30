71e4e9e0dc5de79f166610b176c0e1f11124a676
hey-ri <kwonhr01@naver.com>
2025-04-30 11:47:34 +0900

    2*n 타일링
    다른 풀이

--- 2ntiling.js
+++ 2ntiling.js
@@ -18,3 +18,25 @@ function solution(n) {
 }
 
 console.log(solution(4)); //5
+
+//다른 사람 풀이 <= 지금 테스트에서는 효율성 테스트에서 시간 실패뜸
+function solution(n) {
+  const arr = [0, 1, 2];
+  for (let i = 3; i <= n; i++) {
+    arr[i] = (arr[i - 2] + arr[i - 1]) % 1000000007;
+  }
+
+  return arr[n];
+}
+
+//이거는 효율성 테스트에서 한가지 문제만 시간 초과 걸림
+function solution(n) {
+  const MOD = 1000000007;
+  const dp = [0, 1, 2]; // dp[1] = 1, dp[2] = 2로 초기화
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+  }
+
+  return dp[n];
+}
