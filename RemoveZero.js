edc230b8074477093a0431a7fa7ed14c34d43521
hey-ri <kwonhr01@naver.com>
2025-05-09 14:37:14 +0900

    0 개수 세기

--- RemoveZero.js
+++ RemoveZero.js
@@ -12,4 +12,23 @@ function solution(array) {
   return array.filter((n) => n !== 0);
 }
 
+//제거한 0의 개수 세기
+function solution(array) {
+  return array.filter((n) => n == 0).length;
+}
+
+function solution(array) {
+  return array.reduce((a, c) => {
+    if (c == 0) {
+      a++;
+    }
+
+    return a;
+  }, 0);
+}
+
+function solution(array) {
+  return array.map((n) => (n === 0 ? 1 : undefined)).filter((n) => n !== undefined).length;
+}
+
 console.log(solution([1, 0, 2, 0, 0, 3, 4])); //[1, 2, 3, 4]
